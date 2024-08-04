a82b9be94eb2b59a7644c0a62950983761a66882
velikzhanin-av <a.velikzhanin90@gmail.com>
2024-08-04 13:21:59 +0300

    sync

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import express from 'express'
+import cors from 'cors'
 
 export const app = express()
-app.use(express.json())
+app.use(express.json())  // create body any requests
+app.use(cors())  // allow any front make requests endpoints
