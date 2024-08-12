6e555acbeb3500d50567913cc4a2bca6985e185d
velikzhanin-av <a.velikzhanin90@gmail.com>
2024-08-12 21:13:22 +0300

    added version show

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import express from 'express'
 import cors from 'cors'
 import cookieParser from "cookie-parser";
+import {Request, Response} from "express";
 
 export const app = express()
 app.use(express.json())  // create body any requests
 app.use(cors())  // allow any front make requests endpoints
 app.use(cookieParser())  // allow middleware parse cookie
+
+app.get('/', async (req: Request, res: Response) => {
+    res.send({ version: '8.1' });
+});
